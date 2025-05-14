2f5fef6c8db78c550a04f3c8228cf3550be657eb
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-05-14 13:34:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
-URL_A = "" #News RSS Feed Url 2
+API_ID = "28744454" #API ID get it from telegram.org
+API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7" #API HASH get it from telegram.org
+BOT_TOKEN = "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg" #Bot Token get it from @BotFather
+URL_A = "http://myanimelist.net/rss.php?type=news" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "" #Startup Pic
 MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
